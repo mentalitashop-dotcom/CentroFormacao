5661977b8dea57f9314efe1b2792b8b803df7c49
mentalitashop-dotcom <mentalitashop@gmail.com>
2026-06-30 22:59:17 +0100

    Delete backend directory

--- backend/jwtsecret/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  secret: process.env.JWT_SECRET || 'paw8210183-secret'
-};
--- backend/middleware/adminOnly.js
+++
@@ -1,8 +0,0 @@
-// Função auxiliar «adminOnly».
-﻿module.exports = (req, res, next) => {
-  if (req.userRole === 'Funcionario' && req.userEmployeeRole === 'Admin') {
-    return next();
-  }
-
-  return res.status(403).json({ message: 'Acesso restrito a administradores.' });
-};
